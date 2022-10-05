dc382be941bbbcb9e94ca45e4d204ef564fcd265
Adetunji Mofobi <czrotot999@yahoo.com>
2022-10-05 12:59:50 -0500

    this is my first commit

---
+++ Revature.py
@@ -0,0 +1,43 @@
+lst = ["very easy", "very easy", "easy", "easy", "medium", "medium", "hard", "hard"]
+scores=[]
+total_score =[]
+student_scores =[[2, 3, 8, 6, 5, 12, 10, 18], 86]
+value =0
+result =""
+def candidate_status(lst,total_score,Total,student_scores):
+    for x in range(len(lst)):
+        value = marks(lst[x])
+        scores.append(value)
+    total_score.append(scores)
+    total_score.append(Total)
+    #print(student_scores)
+    #print(total_score)
+    if student_scores[1] > total_score[1]:
+        result="disqualified"
+    
+    else:
+        if len(student_scores[0]) < len(lst):
+            result="disqualified"
+        else:
+            for x in range(len(lst)):
+                if student_scores[0][x] > scores[x]:
+                    result ="disqualified"
+                    break
+                else:
+                    result ="qualified"
+    return result
+    
+def marks(difficulty):
+    mark=0
+    if difficulty == "very easy":
+        mark =5
+    elif difficulty =="easy":
+        mark =10
+    elif difficulty =="medium":
+        mark =15 
+    elif difficulty == "hard":
+        mark =20
+    return mark
+
+
+print(candidate_status(lst,total_score,120,student_scores))
